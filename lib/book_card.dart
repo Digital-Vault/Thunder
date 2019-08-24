811fce75770aeb72bc3798cefa497be1bc24f9cc
Brian Smith <brian.smith@riseup.net>
2019-08-24 13:20:27 -0400

    Add favorite button
    Add the capability to the favorite button in home to favorite a book
    in book BLOC. This allows a user to tap on the favorite button on home
    and the book will show up in favorites tab.

--- lib/book_card.dart
+++ lib/book_card.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter_sample/book_bloc.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 import 'api/book.dart';
+import 'book_provider.dart';
 
 /// Height of the book cover image from NY Times API
 const double bookCoverHeight = 495;
@@ -22,55 +24,61 @@ class BookCard extends StatelessWidget {
   final Book book;
 
   @override
-  Widget build(BuildContext context) => Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: const EdgeInsets.all(10),
-        child: _buildCardColumn(book),
+  Widget build(BuildContext context) {
+    final bookBloc = BookProvider.of(context);
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(10),
+      child: _buildCardColumn(bookBloc),
+    );
+  }
+
+  Widget _buildCardColumn(BookBloc bookBloc) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildImage(),
+          _buildPadding(),
+          _buildButtons(bookBloc),
+        ],
+      );
+
+  Widget _buildImage() => FadeInImage.memoryNetwork(
+        placeholder: kTransparentImage,
+        image: book.bookImage,
+        height: bookCoverHeight,
+        width: bookCoverWidth,
+      );
+
+  Widget _buildPadding() => Padding(
+        padding: const EdgeInsets.all(17),
+        child: _buildTitleAndDescription(),
+      );
+
+  Widget _buildTitleAndDescription() => ListTile(
+        title: Text(book.title),
+        subtitle: Text(book.description),
+      );
+
+  Widget _buildButtons(BookBloc bookBloc) => ButtonTheme.bar(
+          child: ButtonBar(
+        children: [
+          _buildBuyButton(),
+          _buildFavoriteButton(bookBloc),
+        ],
+      ));
+
+  Widget _buildBuyButton() => FlatButton(
+        onPressed: () {},
+        child: const Text('Buy book'),
+      );
+
+  Widget _buildFavoriteButton(BookBloc bookBloc) => IconButton(
+        icon: Icon(EvaIcons.heart,
+            color: book.favorite ? Colors.red : Colors.black),
+        onPressed: () {
+          bookBloc.toggleFavorite.add(book.primaryIsbn13);
+        },
       );
 }
-
-Widget _buildCardColumn(Book book) => Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        _buildImage(book),
-        _buildPadding(book),
-        _buildButtons(),
-      ],
-    );
-
-Widget _buildImage(Book book) => FadeInImage.memoryNetwork(
-      placeholder: kTransparentImage,
-      image: book.bookImage,
-      height: bookCoverHeight,
-      width: bookCoverWidth,
-    );
-
-Widget _buildPadding(Book book) => Padding(
-      padding: const EdgeInsets.all(17),
-      child: _buildTitleAndDescription(book),
-    );
-
-Widget _buildTitleAndDescription(Book book) => ListTile(
-      title: Text(book.title),
-      subtitle: Text(book.description),
-    );
-
-Widget _buildButtons() => ButtonTheme.bar(
-        child: ButtonBar(
-      children: [
-        _buildBuyButton(),
-        _buildFavoriteButton(),
-      ],
-    ));
-
-Widget _buildBuyButton() => FlatButton(
-      onPressed: () {},
-      child: const Text('Buy book'),
-    );
-
-Widget _buildFavoriteButton() => IconButton(
-      icon: Icon(EvaIcons.heart),
-      onPressed: () {},
-    );
